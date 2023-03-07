e26cccfced28ddba8ea3c78fc78875b1df3b21b4
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-07 21:01:42 +0600

    summary of this module added

---
+++ summary.js
@@ -0,0 +1,18 @@
+// Summary of this module 
+// FETCH 
+// we need to know 4 things about fetch:
+// 1. you must provide an url to fetch. url can be static or dynamic . 
+// 2. how to convert the fetch return promist into json 
+// 3. how to convert json to data
+// 4. perform operations on data.
+// take a good care on which type of data you are dealing with
+
+// DOM 
+// 1. get data from DOM 
+// 2. create new element and append to parent element
+// 3. dynamically load data based on id.
+
+
+// Array 
+// forEach,map,find,filter, 
+// template string 
